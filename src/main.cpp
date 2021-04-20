5f21cbe560b6e0f2a3a5d40b23ac230c037696bc
Josef Cullhed <josefcullhed@gmail.com>
2021-04-20 10:19:46 +0200

    Breakthrough, we can parse 65000 files in 140 seconds.

--- src/main.cpp
+++ src/main.cpp
@@ -31,9 +31,9 @@ namespace io = boost::iostreams;
 char const TAG[] = "LAMBDA_ALLOC";
 
 // 5mb chunk
-#define CC_PARSER_CHUNK 4024
-#define CC_PARSER_ZLIB_IN 4024
-#define CC_PARSER_ZLIB_OUT 20240
+#define CC_PARSER_CHUNK 1024*1024*5
+#define CC_PARSER_ZLIB_IN 1024*1024*5
+#define CC_PARSER_ZLIB_OUT 1024*1024*5
 
 class CCParser {
 
@@ -42,7 +42,7 @@ public:
 	CCParser(const Aws::S3::S3Client &s3_client, const string &bucket, const string &key);
 	~CCParser();
 
-	void run();
+	string run();
 
 private:
 
@@ -50,12 +50,15 @@ private:
 	string m_key;
 	Aws::S3::S3Client m_s3_client;
 	int m_cur_offset = 0;
+	bool m_continue_inflate = false;
 
 	z_stream m_zstream; /* decompression stream */
 	char *m_z_buffer_in;
 	char *m_z_buffer_out;
 
 	string next_range();
+	void handle_record_chunk(char *data, int len);
+	int unzip_record(char *data, int size);
 	void unzip_chunk(int bytes_in);
 	void handle_chunk(const char *buffer, int len);
 
@@ -68,21 +71,6 @@ CCParser::CCParser(const Aws::S3::S3Client &s3_client, const string &bucket, con
 
 	m_z_buffer_in = new char[CC_PARSER_ZLIB_IN];
 	m_z_buffer_out = new char[CC_PARSER_ZLIB_OUT];
-
-	m_zstream.zalloc = (alloc_func)0;
-	m_zstream.zfree = (free_func)0;
-	m_zstream.opaque = (voidpf)0;
-
-	m_zstream.next_in = Z_NULL;
-	m_zstream.avail_in = 0;
-
-	m_zstream.next_out = Z_NULL;
-	m_zstream.avail_out = 0;
-
-	int err = inflateInit2(&m_zstream, 16+MAX_WBITS);
-	if (err != Z_OK) {
-		cout << "zlib error" << endl;
-	}
 }
 
 CCParser::~CCParser() {
@@ -106,85 +94,87 @@ int inf(FILE *source, FILE *dest)
 {
 	int consumed = 0, consumed_total = 0;
 	int avail_in_start;
-    int ret;
-    unsigned have;
-    z_stream strm;
-    unsigned char in[CHUNK];
-    unsigned char out[CHUNK];
+	int ret;
+	unsigned have;
+	z_stream strm;
+	unsigned char in[CHUNK];
+	unsigned char out[CHUNK];
 
 
-    /* allocate inflate state */
-    strm.zalloc = Z_NULL;
-    strm.zfree = Z_NULL;
-    strm.opaque = Z_NULL;
-    strm.avail_in = 0;
-    strm.next_in = Z_NULL;
-    ret = inflateInit2(&strm, 16);
-    if (ret != Z_OK)
-        return 0;
+	/* allocate inflate state */
+	strm.zalloc = Z_NULL;
+	strm.zfree = Z_NULL;
+	strm.opaque = Z_NULL;
+	strm.avail_in = 0;
+	strm.next_in = Z_NULL;
+	ret = inflateInit2(&strm, 16);
+	if (ret != Z_OK)
+		return 0;
 	
-    /* decompress until deflate stream ends or end of file */
-    do {
+	/* decompress until deflate stream ends or end of file */
+	do {
 
 		avail_in_start = fread(in, 1, CHUNK, source);
-        strm.avail_in = avail_in_start;
-        if (ferror(source)) {
-            (void)inflateEnd(&strm);
-            return -1;
-        }
-        if (strm.avail_in == 0)
-            break;
-        strm.next_in = in;
+		strm.avail_in = avail_in_start;
+		if (ferror(source)) {
+			(void)inflateEnd(&strm);
+			return -1;
+		}
+		if (strm.avail_in == 0)
+			break;
+		strm.next_in = in;
 
-        /* run inflate() on input until output buffer not full */
-        do {
+		/* run inflate() on input until output buffer not full */
+		do {
 
-            strm.avail_out = CHUNK;
-            strm.next_out = out;
+			strm.avail_out = CHUNK;
+			strm.next_out = out;
 
 
 
-            ret = inflate(&strm, Z_NO_FLUSH);
+			ret = inflate(&strm, Z_NO_FLUSH);
 			consumed = (avail_in_start - strm.avail_in);
 			avail_in_start -= consumed;
 			consumed_total += consumed;
-            assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
-            switch (ret) {
-            case Z_NEED_DICT:
-                ret = Z_DATA_ERROR;     /* and fall through */
-            case Z_DATA_ERROR:
-            case Z_MEM_ERROR:
-                (void)inflateEnd(&strm);
-                return -1;
-            }
+			assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+			switch (ret) {
+			case Z_NEED_DICT:
+				ret = Z_DATA_ERROR;	 /* and fall through */
+			case Z_DATA_ERROR:
+			case Z_MEM_ERROR:
+				(void)inflateEnd(&strm);
+				return -1;
+			}
 
-            have = CHUNK - strm.avail_out;
-            if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
-                (void)inflateEnd(&strm);
-                return -1;
-            }
+			have = CHUNK - strm.avail_out;
+			if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
+				(void)inflateEnd(&strm);
+				return -1;
+			}
 
-        } while (strm.avail_out == 0);
+		} while (strm.avail_out == 0);
 
-        /* done when inflate() says it's done */
-    } while (ret != Z_STREAM_END);
+		/* done when inflate() says it's done */
+	} while (ret != Z_STREAM_END);
 
-    /* clean up and return */
-    (void)inflateEnd(&strm);
-    return consumed_total;
+	//cout << "ret: " << ret << endl;
+
+	/* clean up and return */
+	(void)inflateEnd(&strm);
+	return consumed_total;
 }
 
-void CCParser::run() {
-
+string CCParser::run() {
 
 	Aws::S3::Model::GetObjectRequest request;
 	request.SetBucket(m_bucket);
 	request.SetKey(m_key);
 
 	// Main loop
+	auto total_start = std::chrono::high_resolution_clock::now();
 	while (true) {
 		auto start = std::chrono::high_resolution_clock::now();
-		cout << "fetching range: " << next_range() << endl;
+		//cout << "fetching range: " << next_range() << endl;
 		request.SetRange(next_range());
 
 		m_cur_offset += CC_PARSER_CHUNK;
@@ -196,8 +186,6 @@ void CCParser::run() {
 
 			stream.read(m_z_buffer_in, CC_PARSER_ZLIB_IN);
 
-			cout << "gcount: " << stream.gcount() << endl;
-
 			unzip_chunk(stream.gcount());
 
 		} else {
@@ -205,22 +193,133 @@ void CCParser::run() {
 			cout << outcome.GetError().GetMessage() << endl;
 			break;
 		}
-		//elapsed = std::chrono::high_resolution_clock::now() - start;
-		//microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
-
-		//cout << "second took: " << microseconds / 1000 << " milliseconds" << endl;
-		break;
-
+		auto elapsed = std::chrono::high_resolution_clock::now() - start;
+		auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+		cout << "offset: " << m_cur_offset << " took: " << microseconds / 1000 << " milliseconds" << endl;
+		if (m_cur_offset > 1024*1024*100) {
+			break;
+		}
 	}
+
+	auto total_elapsed = std::chrono::high_resolution_clock::now() - total_start;
+	auto total_microseconds = std::chrono::duration_cast<std::chrono::microseconds>(total_elapsed).count();
+
+	return string("We read ") + to_string(m_cur_offset) + " bytes in total of " + to_string(total_microseconds / 1000) + "ms";
+}
+
+void CCParser::handle_record_chunk(char *data, int len) {
+	//cout << string(data, len);
+}
+
+int CCParser::unzip_record(char *data, int size) {
+
+	int data_offset = 0;
+	int data_size = size;
+	int consumed = 0, consumed_total = 0;
+	int avail_in_start;
+	int ret;
+	unsigned have;
+	z_stream strm;
+	unsigned char out[CHUNK];
+
+	if (!m_continue_inflate) {
+		m_zstream.zalloc = Z_NULL;
+		m_zstream.zfree = Z_NULL;
+		m_zstream.opaque = Z_NULL;
+
+		m_zstream.avail_in = 0;
+		m_zstream.next_in = Z_NULL;
+
+		int err = inflateInit2(&m_zstream, 16);
+		if (err != Z_OK) {
+			cout << "zlib error" << endl;
+		}
+	} else {
+		// just continue on the last one.
+	}
+
+	/* decompress until deflate stream ends or end of file */
+	do {
+
+		m_zstream.next_in = (unsigned char *)(data + data_offset);
+		avail_in_start = min(CHUNK, data_size);
+		m_zstream.avail_in = avail_in_start;
+
+		if (m_zstream.avail_in == 0)
+			break;
+
+		/* run inflate() on input until output buffer not full */
+		do {
+
+			m_zstream.avail_out = CHUNK;
+			m_zstream.next_out = out;
+
+			ret = inflate(&m_zstream, Z_NO_FLUSH);
+			consumed = (avail_in_start - m_zstream.avail_in);
+			data_size -= consumed;
+			data_offset += consumed;
+			avail_in_start -= consumed;
+			consumed_total += consumed;
+			assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+			switch (ret) {
+			case Z_NEED_DICT:
+				ret = Z_DATA_ERROR;	 /* and fall through */
+			case Z_DATA_ERROR:
+			case Z_MEM_ERROR:
+				cout << "Z_MEM_ERROR" << endl;
+				(void)inflateEnd(&m_zstream);
+				return -1;
+			}
+
+			have = CHUNK - m_zstream.avail_out;
+			handle_record_chunk((char *)out, have);
+
+		} while (m_zstream.avail_out == 0);
+
+		/* done when inflate() says it's done */
+	} while (ret != Z_STREAM_END);
+
+	//cout << "ret: " << ret << endl;
+	if (ret == 0) {
+		m_continue_inflate = true;
+	} else {
+		m_continue_inflate = false;
+		(void)inflateEnd(&m_zstream);
+	}
+
+	/* clean up and return */
+	return consumed_total;
 }
 
 void CCParser::unzip_chunk(int bytes_in) {
 
+	int consumed = 0;
+	int consumed_total = 0;
+
+	char *ptr = m_z_buffer_in;
+	int len = bytes_in;
+
+	while (len > 0) {
+		consumed = unzip_record(ptr, len);
+		//cout << "consumed: " << consumed << " len: " << len << endl;
+		if (consumed == 0) {
+			cout << "Nothing consumed, done..." << endl;
+			break;
+		}
+		if (consumed < 0) {
+			cout << "Encountered fatal error" << endl;
+			break;
+		}
+		ptr += consumed;
+		len -= consumed;
+		consumed_total += consumed;
+	}
+
+	/*
 	int ret, bytes_out;
 
 	m_zstream.next_in = (unsigned char *)m_z_buffer_in;
 	m_zstream.avail_in = bytes_in;
-	cout << bytes_in << endl;
 	do {
 		m_zstream.avail_out = CC_PARSER_ZLIB_OUT;
 		cout << "avail_out: " << m_zstream.avail_out << endl;
@@ -248,6 +347,7 @@ void CCParser::unzip_chunk(int bytes_in) {
 		cout << "avail_out: " << m_zstream.avail_out << endl;
 
 	} while (m_zstream.avail_out == 0);
+	*/
 
 }
 
@@ -278,16 +378,16 @@ static invocation_response my_handler(invocation_request const& req, Aws::S3::S3
 	AWS_LOGSTREAM_INFO(TAG, "Attempting to download file from s3://" << bucket << "/" << key);
 
 	CCParser parser(client, bucket, key);
-	parser.run();
+	string response = parser.run();
 
-	return invocation_response::success("We did it!", "application/base64");
+	return invocation_response::success("We did it! Response: " + response, "application/base64");
 }
 
 std::function<std::shared_ptr<Aws::Utils::Logging::LogSystemInterface>()> GetConsoleLoggerFactory()
 {
 	return [] {
 		return Aws::MakeShared<Aws::Utils::Logging::ConsoleLogSystem>(
-			"console_logger", Aws::Utils::Logging::LogLevel::Trace);
+			"console_logger", Aws::Utils::Logging::LogLevel::Info);
 	};
 }
 
@@ -306,12 +406,14 @@ void run_lambda_handler() {
 }
 
 int main(int argc, const char **argv) {
-	FILE *fp_in = fopen("CC-MAIN-20210228145113-20210228175113-00135.warc.gz", "rb");
+	/*FILE *fp_in = fopen("asd.gz", "rb");
 	FILE *fp_out = fopen("out.warc", "wb");
 	int consumed = 0;
 	int consumed_total = 0;
 	while (true) {
 		consumed = inf(fp_in, fp_out);
+		cout << "consumed: " << consumed << endl;
+		break;
 		if (consumed == 0) {
 			cout << "Nothing consumed, done..." << endl;
 			break;
@@ -325,19 +427,20 @@ int main(int argc, const char **argv) {
 	}
 	fclose(fp_in);
 	fclose(fp_out);
-	return 0;
+	return 0;*/
 	Aws::SDKOptions options;
+	options.loggingOptions.logLevel = Aws::Utils::Logging::LogLevel::Info;
+	options.loggingOptions.logger_create_fn = GetConsoleLoggerFactory();
 	Aws::InitAPI(options);
 
-	//run_lambda_handler();
-	/*if (argc == 1) {
-	} else {
-	*/
+	run_lambda_handler();
+
+	/*
 	Aws::Client::ClientConfiguration clientConfig;
 	clientConfig.region = "us-east-1";
 	CCParser parser(Aws::S3::S3Client(clientConfig), "commoncrawl", "crawl-data/CC-MAIN-2021-10/segments/1614178361510.12/warc/CC-MAIN-20210228145113-20210228175113-00135.warc.gz");
-	parser.run();
-	//}
+	cout << parser.run();
+	*/
 
 	Aws::ShutdownAPI(options);
 
