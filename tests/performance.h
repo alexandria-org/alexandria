84f4ef3b4824d166f9e3a626acfd0f909d237309
Josef Cullhed <josefcullhed@gmail.com>
2021-09-18 18:38:31 +0200

    Facebook search in under 1000ms.

--- tests/performance.h
+++ tests/performance.h
@@ -7,16 +7,31 @@ BOOST_AUTO_TEST_SUITE(performance)
 BOOST_AUTO_TEST_CASE(domain_index_sharp) {
 
 	struct SearchMetric metric;
-	string query = "facebook";
+	string query = "google";
 
+	HashTable hash_table("main_index");
+	HashTable hash_table_link("link_index");
+	HashTable hash_table_domain_link("domain_link_index");
+
+	vector<FullTextIndex<FullTextRecord> *> index_array = FullText::create_index_array<FullTextRecord>("main_index", 8);
+	vector<FullTextIndex<LinkFullTextRecord> *> link_index_array = FullText::create_index_array<LinkFullTextRecord>("link_index", 8);
 	vector<FullTextIndex<DomainLinkFullTextRecord> *> domain_link_index_array =
 		FullText::create_index_array<DomainLinkFullTextRecord>("domain_link_index", 8);
+
+	Profiler::instance profiler_total("TOTAL");
 	
-	Profiler::instance profiler_domain_links("SearchEngine::search_domain_link_array");
+	Profiler::instance profiler_links("SearchEngine::search<LinkFullTextRecord>");
+	vector<LinkFullTextRecord> links = SearchEngine::search<LinkFullTextRecord>(link_index_array, query, 500000, metric);
+	profiler_links.stop();
+
+	Profiler::instance profiler_domain_links("SearchEngine::search<DomainLinkFullTextRecord>");
 	vector<DomainLinkFullTextRecord> domain_links = SearchEngine::search<DomainLinkFullTextRecord>(domain_link_index_array, query, 10000, metric);
 	profiler_domain_links.stop();
 
-	
+	Profiler::instance profiler_index("SearchEngine::search_with_links");
+	vector<FullTextRecord> results = SearchEngine::search_with_links(index_array, links, domain_links, query, 1000, metric);
+	profiler_index.stop();
+
 }
 
 BOOST_AUTO_TEST_CASE(domain_index) {
