a8f168af5323adf5197e3aaae745390e7df6be48
joscul <josefcullhed@gmail.com>
2021-10-30 09:52:50 +0200

    Updated bootstrap node scripts.

--- scripts/bootstrap_node_2drives.sh
+++ scripts/bootstrap_node_2drives.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get -y install vim parted screen zip
+apt-get -y install vim parted zip nginx
 
 echo "\nALEXANDRIA_LIVE=1" >> /etc/environment
 
@@ -56,4 +56,31 @@ for shard in $(seq 0 7); do
 	mkdir "/mnt/$shard/full_text";
 	mkdir "/mnt/$shard/tmp";
 done
+
+echo "server {
+    listen 80;
+    server_name localhost;
+
+    location / {
+        fastcgi_pass   127.0.0.1:8000;
+        fastcgi_param  GATEWAY_INTERFACE  CGI/1.1;
+        fastcgi_param  SERVER_SOFTWARE    nginx;
+        fastcgi_param  QUERY_STRING       \$query_string;
+        fastcgi_param  REQUEST_METHOD     \$request_method;
+        fastcgi_param  CONTENT_TYPE       \$content_type;
+        fastcgi_param  CONTENT_LENGTH     \$content_length;
+        fastcgi_param  SCRIPT_FILENAME    \$document_root\$fastcgi_script_name;
+        fastcgi_param  SCRIPT_NAME        \$fastcgi_script_name;
+        fastcgi_param  REQUEST_URI        \$request_uri;
+        fastcgi_param  DOCUMENT_URI       \$document_uri;
+        fastcgi_param  DOCUMENT_ROOT      \$document_root;
+        fastcgi_param  SERVER_PROTOCOL    \$server_protocol;
+        fastcgi_param  REMOTE_ADDR        \$remote_addr;
+        fastcgi_param  REMOTE_PORT        \$remote_port;
+        fastcgi_param  SERVER_ADDR        \$server_addr;
+        fastcgi_param  SERVER_PORT        \$server_port;
+        fastcgi_param  SERVER_NAME        \$server_name;
+    }
+}" > /etc/nginx/sites-enabled/default
+/etc/init.d/nginx restart
 
