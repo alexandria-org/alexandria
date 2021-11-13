22d2c5cece4374f8a738626d4b387aad509ebd96
Josef Cullhed <josefcullhed@gmail.com>
2021-11-14 00:22:08 +0100

    Changed to 2 partitions in default config.

--- src/config.h
+++ src/config.h
@@ -27,7 +27,7 @@ namespace Config {
 
 	// Full text indexer config
 	inline const unsigned long long ft_num_shards = 1024;
-	inline const unsigned long long ft_num_partitions = 1;
+	inline const unsigned long long ft_num_partitions = 2;
 	inline const unsigned long long ft_max_keys = 0xFFFFFFFF;
 	inline const unsigned long long ft_max_cache_gb = 30;
 	inline const unsigned long long ft_num_threads_indexing = 24;
