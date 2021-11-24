b4213759a086f31a3fb9c5402fc183a6a75990c2
Josef Cullhed <josefcullhed@gmail.com>
2021-11-24 08:30:01 +0100

    Added update script.

--- scripts/packager.sh
+++ scripts/packager.sh
@@ -158,8 +158,10 @@ EOF
 cp "$PKG_BIN_PATH/server" "$PKG_DIR/bin"
 cp "$PKG_BIN_PATH/../scripts/bootstrap_node_2drives.sh" "$PKG_DIR/"
 cp "$PKG_BIN_PATH/../scripts/truncate.sh" "$PKG_DIR/"
+cp "$PKG_BIN_PATH/../scripts/update.sh" "$PKG_DIR/"
 chmod +x "$PKG_DIR/bootstrap_node_2drives.sh"
 chmod +x "$PKG_DIR/truncate.sh"
+chmod +x "$PKG_DIR/update.sh"
 echo -e "$bootstrap_script_server" > "$PKG_DIR/server"
 chmod +x "$PKG_DIR/server"
 # some shenanigans to create the right layout in the zip file without extraneous directories
---
+++ scripts/update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+wget https://github.com/alexandria-org/alexandria/releases/latest/download/alexandria.zip -O alexandria.zip
+unzip -o alexandria.zip
+
