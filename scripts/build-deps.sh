24e7f617448637e16fd869f640744bc15a9208a6
joscul <josefcullhed@gmail.com>
2021-12-13 10:11:38 +0100

    Removed all aws deps.

--- scripts/build-deps.sh
+++ scripts/build-deps.sh
@@ -16,22 +16,3 @@ cd zlib-1.2.11
 make -j4
 make install
 
-cd $base_path
-cd deps
-
-cd aws-lambda-cpp
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DCMAKE_INSTALL_PREFIX=../../out -DCPP_STANDARD=17
-make -j4 && make install
-
-cd $base_path
-cd deps
-
-cd aws-sdk-cpp
-mkdir build
-cd build
-cmake .. -DBUILD_ONLY="s3;transfer;lambda" -DBUILD_SHARED_LIBS=OFF -DENABLE_UNITY_BUILD=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../out -DENABLE_TESTING=OFF -DBUILD_DEPS=ON -DCPP_STANDARD=17 -DAWS_LIBCRYPTO_LOG_RESOLVE=0
-make -j4
-make install
-
--- scripts/download-deps.sh
+++ scripts/download-deps.sh
@@ -18,16 +18,3 @@ curl https://zlib.net/zlib-1.2.11.tar.gz > zlib-1.2.11.tar.gz
 gunzip zlib-1.2.11.tar.gz
 tar -xvf zlib-1.2.11.tar
 
-cd $base_path
-cd deps
-
-git clone https://github.com/awslabs/aws-lambda-cpp.git
-
-cd $base_path
-cd deps
-
-git clone https://github.com/aws/aws-sdk-cpp.git
-cd aws-sdk-cpp
-#git checkout tags/1.9.47
-git submodule update --init --recursive
-
