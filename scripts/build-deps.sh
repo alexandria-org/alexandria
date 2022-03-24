b343f3096dad4c06b66809b624d381fcedca4a35
= <josefcullhed@gmail.com>
2022-03-24 07:21:17 +0100

    Added CRoaring to build-deps.sh script.

--- scripts/build-deps.sh
+++ scripts/build-deps.sh
@@ -7,12 +7,22 @@ export CC=/usr/bin/gcc-10
 export CXX=/usr/bin/g++-10
 
 base_path=`pwd`
-cd $base_path
 
+cd $base_path
 cd deps
 
 cd zlib-1.2.11
 ./configure
 make -j4
+make install
+
+cd $base_path
+cd deps
+
+cd CRoaring
+mkdir build
+cd build
+cmake ..
+make
 make install
 
