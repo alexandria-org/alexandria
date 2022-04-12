e46c659868f3c9db1bc5a3a5bc03abe3a09cdfd8
Josef Cullhed <josefcullhed@gmail.com>
2022-04-12 14:25:43 +0200

    Update install-deps.sh

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apt-get install -y zip make cmake gcc-10 g++-10 libcurl4-openssl-dev libssl-dev libcrypto++-dev libboost-iostreams-dev libboost-filesystem-dev libboost-system-dev libboost-test-dev libfcgi-dev spawn-fcgi nginx
+apt-get install -y zip make cmake gcc-10 g++-10 gcc g++ libcurl4-openssl-dev libssl-dev libcrypto++-dev libboost-iostreams-dev libboost-filesystem-dev libboost-system-dev libboost-test-dev libfcgi-dev spawn-fcgi nginx
